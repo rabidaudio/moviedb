ede03371201f140f6bf9b6f9fbdca5d84c193fbf
Charles Julian Knight <charles@rabidaudio.com>
2015-02-04 15:32:00 -0500

    mini movie view

--- app/controllers/viewings_controller.rb
+++ app/controllers/viewings_controller.rb
@@ -15,8 +15,23 @@ class ViewingsController < ApplicationController
     redirect_to @viewing
   end
 
+  def index
+    if params[:movie_id]
+      @movie = Movie.find(params[:movie_id])
+      @viewings = @movie.viewings
+    elsif params[:user_id]
+      @user = User.find(params[:user_id])
+      @viewings = @user.viewings
+    else
+      @viewings = Viewings.all
+    end
+
+    @viewings  =   if params[:user_id]
+  end
+
 
   private
+  # convert form data to usable params
   def viewing_params
      params.require(:viewing).permit(:rating, :comments, :format, :first_time).tap do |v|
       v[:rating] = v[:rating].to_i
